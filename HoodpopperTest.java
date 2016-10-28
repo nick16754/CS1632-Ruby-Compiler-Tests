67f5ef7c48e59a6f4444adeac459b06e8606be20
Michael Oles <mbo10@pitt.edu>
2016-10-28 17:37:19 -0400

    new tests

--- HoodpopperTest.java
+++ HoodpopperTest.java
@@ -39,7 +39,7 @@ public class HoodpopperTest {
     String elementText = e.getText();
     assertTrue(elementText.contains("Compile"));
   }
-  
+
   @Test
   public void testTokenizeHasBackLink() {
 	driver.findElement(By.name("commit")).click();
@@ -49,7 +49,7 @@ public class HoodpopperTest {
 		fail();
 	}
   }
-  
+
   @Test
   public void testParseHasBackLink() {
 	driver.findElement(By.xpath("(//input[@name='commit'])[2]")).click();
@@ -59,7 +59,7 @@ public class HoodpopperTest {
 		fail();
 	}
   }
-  
+
   @Test
   public void testCompileHasBackLink() {
 	driver.findElement(By.xpath("(//input[@name='commit'])[3]")).click();
@@ -68,6 +68,70 @@ public class HoodpopperTest {
 	} catch (NoSuchElementException nseex) {
 		fail();
 	}
+  }
+
+  @Test
+  public void tokenizeSpace(){
+    driver.findElement(By.id("code_code")).clear();
+    driver.findElement(By.id("code_code")).sendKeys("a = 1");
+    driver.findElement(By.name("commit")).click();
+    WebElement e = driver.findElement(By.cssSelector("p"));
+    String elementText = e.getText();
+    assertTrue(elementText.contains(":on_sp"));
+  }
+
+  @Test
+  public void tokenizeNoSpace(){
+    driver.findElement(By.id("code_code")).clear();
+    driver.findElement(By.id("code_code")).sendKeys("a=1");
+    driver.findElement(By.name("commit")).click();
+    WebElement e = driver.findElement(By.cssSelector("p"));
+    String elementText = e.getText();
+    assertFalse(elementText.contains(":on_sp"));
+  }
+
+  @Test
+  public void testTokenizeIgnoredNewline(){
+    driver.findElement(By.id("code_code")).clear();
+    driver.findElement(By.id("code_code")).sendKeys("puts hello;\nx = 5;");
+    driver.findElement(By.name("commit")).click();
+    WebElement e = driver.findElement(By.cssSelector("p"));
+    String elementText = e.getText();
+    assertFalse(elementText.contains(":on_nl"));
+    assertTrue(elementText.contains(":on_ignored_nl"));
+  }
+
+  @Test
+  public void testTokenizeNewline(){
+    driver.findElement(By.id("code_code")).clear();
+    driver.findElement(By.id("code_code")).sendKeys("puts hello\nx = 5;");
+    driver.findElement(By.name("commit")).click();
+    WebElement e = driver.findElement(By.cssSelector("p"));
+    String elementText = e.getText();
+    assertTrue(elementText.contains(":on_nl"));
+    assertFalse(elementText.contains(":on_ignored_nl"));
+  }
+
+  @Test
+  public void testTokenizeOps(){
+    driver.findElement(By.id("code_code")).clear();
+    driver.findElement(By.id("code_code")).sendKeys("x = 5;\nx = x + 7\nx = x/3;");
+    driver.findElement(By.name("commit")).click();
+    WebElement e = driver.findElement(By.cssSelector("p"));
+    String elementText = e.getText();
+    assertTrue(elementText.contains(":on_op, \"=\""));
+    assertTrue(elementText.contains(":on_op, \"+\""));
+    assertTrue(elementText.contains(":on_op, \"/\""));
+  }
+
+  @Test
+  public void testTokenizeNoOps(){
+    driver.findElement(By.id("code_code")).clear();
+    driver.findElement(By.id("code_code")).sendKeys("puts hello");
+    driver.findElement(By.name("commit")).click();
+    WebElement e = driver.findElement(By.cssSelector("p"));
+    String elementText = e.getText();
+    assertFalse(elementText.contains(":on_op"));
   }
 
 }
