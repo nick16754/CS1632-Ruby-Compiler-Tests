ed7221a6882a0d999bb6ad2f37d8d3b2e14e31ba
Michael Oles <mbo10@pitt.edu>
2016-10-30 12:00:24 -0400

    Improve comments, Improve back button text

--- HoodpopperTest.java
+++ HoodpopperTest.java
@@ -21,6 +21,7 @@ public class HoodpopperTest {
   // Then I see that it contains the word "Hoodpopper"
   @Test
 	public void testShowsCorrectTitle() {
+		//Check the title as soon at the site loads
 		String title = driver.getTitle();
 		assertTrue(title.contains("Hoodpopper"));
 	}
@@ -31,6 +32,7 @@ public class HoodpopperTest {
   // "Hood Popped - Tokenize Operation" which contains the word "Tokenize"
   @Test
   public void testTokenizeButton() {
+	// Click on Tokenize button
     driver.findElement(By.name("commit")).click();
     WebElement e = driver.findElement(By.cssSelector("h1"));
     String elementText = e.getText();
@@ -43,6 +45,7 @@ public class HoodpopperTest {
   // "Hood Popped - Parse Operation" which contains the word "Parse"
   @Test
   public void testParseButton() {
+	// Click on Parse button
     driver.findElement(By.xpath("(//input[@name='commit'])[2]")).click();
     WebElement e = driver.findElement(By.cssSelector("h1"));
     String elementText = e.getText();
@@ -55,6 +58,7 @@ public class HoodpopperTest {
   // "Hood Popped - Compile Operation" which contains the word "Compile"
   @Test
   public void testCompileButton() {
+	// Click on Compile
     driver.findElement(By.xpath("(//input[@name='commit'])[3]")).click();
     WebElement e = driver.findElement(By.cssSelector("h1"));
     String elementText = e.getText();
@@ -66,7 +70,9 @@ public class HoodpopperTest {
   // Then I should see that the "Tokenize" page contains the "Back" link
   @Test
   public void testTokenizeHasBackLink() {
+	// Click on Tokenize button
 	driver.findElement(By.name("commit")).click();
+	// Test fails if there is no back link on the page
 	try {
 		driver.findElement(By.linkText("Back"));
 	} catch (NoSuchElementException nseex) {
@@ -80,6 +86,7 @@ public class HoodpopperTest {
   @Test
   public void testParseHasBackLink() {
 	driver.findElement(By.xpath("(//input[@name='commit'])[2]")).click();
+	// Test fails if there is no back link on the page	  
 	try {
 		driver.findElement(By.linkText("Back"));
 	} catch (NoSuchElementException nseex) {
@@ -93,6 +100,7 @@ public class HoodpopperTest {
   @Test
   public void testCompileHasBackLink() {
 	driver.findElement(By.xpath("(//input[@name='commit'])[3]")).click();
+	// Test fails if there is no back link on the page	  
 	try {
 		driver.findElement(By.linkText("Back"));
 	} catch (NoSuchElementException nseex) {
@@ -105,10 +113,22 @@ public class HoodpopperTest {
   // Then I should be redirected from the "Tokenize" page to the main page
   @Test
   public void testBackLink() {
+	// Click on Tokenize button	  
     driver.findElement(By.name("commit")).click();
+	// Checks that there is no parse link (confirming we left the main page)
+	try {
+		driver.findElement(By.name("commit"));
+		fail();
+	} catch (NoSuchElementException nseex) {
+		// Expected result because there should be no link on this apge
+	}  
     driver.findElement(By.linkText("Back")).click();
-    String title = driver.getTitle();
-	assertTrue(title.contains("Hoodpopper"));
+	// Checks that there is a parse link (confirming we have returned to the main page)
+	try {
+		driver.findElement(By.name("commit"));
+	} catch (NoSuchElementException nseex) {
+		fail();
+	}
   }
   
   // Given that I am on the main page
@@ -118,6 +138,7 @@ public class HoodpopperTest {
   public void testFunctionPutsIsIdentifier() {
 	driver.findElement(By.id("code_code")).clear();
     driver.findElement(By.id("code_code")).sendKeys("puts");
+	// Click on Tokenize button	  
     driver.findElement(By.name("commit")).click();
 	WebElement e = driver.findElement(By.cssSelector("p"));
 	String tokenized = e.getText();
@@ -131,6 +152,7 @@ public class HoodpopperTest {
   public void testVariableAIsIdentifier() {
 	driver.findElement(By.id("code_code")).clear();
     driver.findElement(By.id("code_code")).sendKeys("a");
+	// Click on Tokenize button	  
     driver.findElement(By.name("commit")).click();
 	WebElement e = driver.findElement(By.cssSelector("p"));
 	String tokenized = e.getText();
@@ -143,82 +165,107 @@ public class HoodpopperTest {
   // Then "putsa" should be tokenized as an identifier
   @Test
   public void testVariablePutsaIsIdentifier() {
-	driver.findElement(By.id("code_code")).clear();
+    driver.findElement(By.id("code_code")).clear();
     driver.findElement(By.id("code_code")).sendKeys("putsa");
+	// Click on Tokenize button	  
     driver.findElement(By.name("commit")).click();
-	WebElement e = driver.findElement(By.cssSelector("p"));
-	String tokenized = e.getText();
-	assertTrue(tokenized.contains(":on_ident, \"putsa\""));
+    WebElement e = driver.findElement(By.cssSelector("p"));
+    String tokenized = e.getText();
+    assertTrue(tokenized.contains(":on_ident, \"putsa\""));
   }
 
-  //Check whitespace token
+  // Given that I am on the main page
+  // When I submit code that contains a space character and click "Tokenize"
+  // It should create a token called :on_sp
   @Test
   public void tokenizeSpace(){
     driver.findElement(By.id("code_code")).clear();
     driver.findElement(By.id("code_code")).sendKeys("a = 1");
+	// Click on Tokenize button	  
     driver.findElement(By.name("commit")).click();
     WebElement e = driver.findElement(By.cssSelector("p"));
     String elementText = e.getText();
+	// Confirm that is an on_sp token created
     assertTrue(elementText.contains(":on_sp"));
   }
 
-  //Check that no whitespace token is created with no spaces
+  // Given that I am on the main page
+  // When I submit code that contains a space character and click "Tokenize"
+  // It should not create a token called :on_sp
   @Test
   public void tokenizeNoSpace(){
     driver.findElement(By.id("code_code")).clear();
     driver.findElement(By.id("code_code")).sendKeys("a=1");
+	// Click on Tokenize button	  
     driver.findElement(By.name("commit")).click();
     WebElement e = driver.findElement(By.cssSelector("p"));
     String elementText = e.getText();
+	// There should be no on_sp token
     assertFalse(elementText.contains(":on_sp"));
   }
-
-  //When a newline is put after a semicolon there should be no on_nl token created
-  //but there should be an ignored_nl token
+	
+  // Edge Case:  Even when a newline is typed, if its after a semicolon :on_nl token is not created	
+  // Given that I am on the main page and type a newline directly after a semicolon	
+  // When a newline is put after a semicolon in the textbox and "Tokenize" is clicked 
+  // There should be no on_nl token created but there should be an ignored_nl token
   @Test
   public void testTokenizeIgnoredNewline(){
     driver.findElement(By.id("code_code")).clear();
     driver.findElement(By.id("code_code")).sendKeys("puts hello;\nx = 5;");
+	// Click on Tokenize button	  
     driver.findElement(By.name("commit")).click();
     WebElement e = driver.findElement(By.cssSelector("p"));
     String elementText = e.getText();
+	// Only the on_ignored_nl token is created
     assertFalse(elementText.contains(":on_nl"));
     assertTrue(elementText.contains(":on_ignored_nl"));
   }
 
-  //When a newline is put in the code, newline should be tokenized
+  // Given that I am on the main page and type a newline directly after a semicolon	
+  // When a newline is entered and I click "Tokenize" 
+  // There should be an on_nl token created but there should not be an any ignored_nl tokens
   @Test
   public void testTokenizeNewline(){
     driver.findElement(By.id("code_code")).clear();
     driver.findElement(By.id("code_code")).sendKeys("puts hello\nx = 5;");
+	// Click on Tokenize button	  
     driver.findElement(By.name("commit")).click();
     WebElement e = driver.findElement(By.cssSelector("p"));
     String elementText = e.getText();
+	// Only the on_nl token is created
     assertTrue(elementText.contains(":on_nl"));
     assertFalse(elementText.contains(":on_ignored_nl"));
   }
 
-  //Check that +, /, and = operations all are tokenized
+  // Given that I type in any code that conatins operators (+,-,*,/,=)
+  // When I clicked tokenize on the main page 
+  // The page should display an :on_op token with that opertor in quotes
   @Test
   public void testTokenizeOps(){
     driver.findElement(By.id("code_code")).clear();
     driver.findElement(By.id("code_code")).sendKeys("x = 5;\nx = x + 7\nx = x/3;");
+	// Click on Tokenize button	  
     driver.findElement(By.name("commit")).click();
     WebElement e = driver.findElement(By.cssSelector("p"));
     String elementText = e.getText();
+	// Text should contain each specific operator token
     assertTrue(elementText.contains(":on_op, \"=\""));
     assertTrue(elementText.contains(":on_op, \"+\""));
     assertTrue(elementText.contains(":on_op, \"/\""));
   }
 
-  //When no operations are in the code there should be no :on_op token
+  // Given that I type in any code that does not contain operators (+,-,*,/,=)
+  // When I clicked tokenize on the main page 
+  // The page should not display any :on_op tokens
   @Test
   public void testTokenizeNoOps(){
     driver.findElement(By.id("code_code")).clear();
     driver.findElement(By.id("code_code")).sendKeys("puts hello");
+	// Click on Tokenize button	  
     driver.findElement(By.name("commit")).click();
     WebElement e = driver.findElement(By.cssSelector("p"));
     String elementText = e.getText();
+	// Text should not contain operator token
     assertFalse(elementText.contains(":on_op"));
   }
 
@@ -230,11 +277,13 @@ public class HoodpopperTest {
   public void testParseOnlyOperator(){
 	driver.findElement(By.id("code_code")).clear();
     driver.findElement(By.id("code_code")).sendKeys("+");
+	// Test fails if there is no parse element	  
 	try {
-	driver.findElement(By.xpath("(//input[@name='commit'])[2]")).click();
-	WebElement e = driver.findElement(By.xpath("//p[2]"));
-	String AST = e.getText();
-	assertTrue(AST.contains("+"));
+		// Click on Parse button
+	    driver.findElement(By.xpath("(//input[@name='commit'])[2]")).click();
+	    WebElement e = driver.findElement(By.xpath("//p[2]"));
+	    String AST = e.getText();
+	    assertTrue(AST.contains("+"));
 	} catch(NoSuchElementException nseex) {
 		fail();
 	}
@@ -249,7 +298,9 @@ public class HoodpopperTest {
   public void testParseOnlySpecialChar() {
 	driver.findElement(By.id("code_code")).clear();
     driver.findElement(By.id("code_code")).sendKeys("!");
+	// Test fails if there is no parse element	  
 	try {
+		// Click on Parse button		
 		driver.findElement(By.xpath("(//input[@name='commit'])[2]")).click();
 		WebElement e = driver.findElement(By.xpath("//p[2]"));
 		String AST = e.getText();
@@ -265,31 +316,39 @@ public class HoodpopperTest {
   // Then I should see all three types shown in the AST
   @Test
   public void testParseShowsCorrectTokensInAST() {
-	driver.findElement(By.id("code_code")).clear();
+    driver.findElement(By.id("code_code")).clear();
     driver.findElement(By.id("code_code")).sendKeys("puts + 3.14159");
-	try {
+	// Test fails if there is no parse element
+    try {
+		// Click on Parse button		
 		driver.findElement(By.xpath("(//input[@name='commit'])[2]")).click();
 		WebElement e = driver.findElement(By.xpath("//p[2]"));
 		String AST = e.getText();
+		// Confirm operator, value and command are all in the tree
 		assertTrue(AST.contains("puts"));	
 		assertTrue(AST.contains("+"));
 		assertTrue(AST.contains("3.14159"));
-	} catch(NoSuchElementException nseex) {
+    } catch(NoSuchElementException nseex) {
 		fail();
-	}
+    }
   }
 
-
-//Check that the tree is the same for two different inputs when the only
-//difference between the imputs its added whitespace
+  // Given that I am on the main page
+  // When I enter two different inputs where the only difference between inputs
+  // Is added whitespace tokens and click on "Parse"
+  // Then whitespace should be ignored and both should return the same tree
   @Test
   public void testParseWhitespace(){
+	// Save the result from parsing the first string
     driver.findElement(By.id("code_code")).clear();
     driver.findElement(By.id("code_code")).sendKeys("puts helloworld;");
+	// Click on Parse button	  
     driver.findElement(By.xpath("(//input[@name='commit'])[2]")).click();
     WebElement e = driver.findElement(By.xpath("//p[2]"));
     String noWhitespaceText = e.getText();
 
+	// Create a new driver at the same location as driver
+	// Save the result from parsing thee second string
     WebDriver newDriver = new HtmlUnitDriver();
     newDriver.get("http://lit-bayou-7912.herokuapp.com/");
     newDriver.findElement(By.id("code_code")).clear();
@@ -297,40 +356,54 @@ public class HoodpopperTest {
     newDriver.findElement(By.xpath("(//input[@name='commit'])[2]")).click();
     e = newDriver.findElement(By.xpath("//p[2]"));
     String whitespaceText = e.getText();
+	  
+	// Check the strings are the same both times  
     assertEquals(noWhitespaceText,whitespaceText);
   }
 
-  //checks that the use of puts in the code compiles putstring in YARV
+  // Given that I am on that main page of Hoodpopper
+  // When I enter the puts command in the textbox and click "Compile"
+  // Then the instruction putstring should be part of the in YARV code
   @Test
   public void testCompilePuts(){
     driver.findElement(By.id("code_code")).clear();
     driver.findElement(By.id("code_code")).sendKeys("puts \"whats up\"");
+	//Click on Compile button  
     driver.findElement(By.xpath("(//input[@name='commit'])[3]")).click();
     WebElement e = driver.findElement(By.cssSelector("p"));
     String elementText = e.getText();
+	// Confirm the compiled code contains putstring
     assertTrue(elementText.contains("putstring"));
   }
 
-  //checks that the use of plus/multiply in the code compiles with opt_plus/opt_mult
+  // Given that I am on the main page of Hoodpopper
+  // When I enter text that contains an operator and click "Compile"
+  // The corresponding opt instruction should appear in the YARV code 
   @Test
   public void testCompileOps(){
     driver.findElement(By.id("code_code")).clear();
     driver.findElement(By.id("code_code")).sendKeys("x = 5 + 3 * 100");
+	// Click on Compile button 	  
     driver.findElement(By.xpath("(//input[@name='commit'])[3]")).click();
     WebElement e = driver.findElement(By.cssSelector("p"));
     String elementText = e.getText();
+	// Confirm the compiled code does contain operations
     assertTrue(elementText.contains("opt_plus"));
     assertTrue(elementText.contains("opt_mult"));
   }
 
-  //code without operations or puts should not cotain opt or putstring instructions
+  // Given that I am on the main page of Hoodpopper
+  // When I enter text that contains no operators or puts commands and click "Compile"
+  // Then no opt or putstring instructions should appear in the YARV code
   @Test
   public void testCompileEmpty(){
     driver.findElement(By.id("code_code")).clear();
     driver.findElement(By.id("code_code")).sendKeys("x;\n");
+	// Click on Compile button 	  
     driver.findElement(By.xpath("(//input[@name='commit'])[3]")).click();
     WebElement e = driver.findElement(By.cssSelector("p"));
     String elementText = e.getText();
+	// Check that the compiled code does not contain putstring or operations
     assertFalse(elementText.contains("putstring"));
     assertFalse(elementText.contains("opt_plus"));
     assertFalse(elementText.contains("opt_mult"));
